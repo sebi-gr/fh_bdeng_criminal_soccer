7dc8236f2e1c14500d53e38600f475698730d213
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2023-06-23 13:04:55 +0200

    improvements

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ MONGO_DB_REMOTE = "criminalSoccer"
 
 MONGO_HOST_LOCAL = "mongodb://localhost:27017/"
 MONGO_DB_LOCAL = "criminalSoccer"
+
+KAFKA_TOPIC = "criminal_soccer_gdp"
+KAFKA_SERVER = "kafka:9092"
